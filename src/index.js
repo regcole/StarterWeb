e11b8f1e177280025bf8ff39c1533e92d33273e2
Reggie Cole <reggie@robokind.com>
2019-01-18 14:43:14 -0600

    start a baseline thing

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function initFirstTree() {
+    let elm = document.getElementById("tree");
+    let schema = {"name":"string"};
+}
+
+window.onload = () => {
+    initFirstTree();
+};
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -5,24 +6,32 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
-        devtool: 'inline-source-map',
-       devServer: {
-         contentBase: './dist'
-       },
+    entry: {
+        main: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-             title: 'Development',
-             template: 'src/index.ejs'
-    }),
+            title: 'Development',
+            template: 'src/index.ejs'
+        }),
         new CopyWebpackPlugin([
-            { from: 'static', to: 'static' },
-        ])
-],
+            {from: 'static', to: 'static'},
+        ]),
+        new webpack.ProvidePlugin({
+            tv4 : 'tv4'
+        }),
+    ],
     output: {
-    filename: '[name].bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    externals: {
+        jquery: 'jQuery',
     },
     module: {
 
@@ -40,6 +49,7 @@ module.exports = {
                     'file-loader'
                 ]
             }
+
         ]
     }
 };
