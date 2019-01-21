1388bd034f2dc4ebb0b3f00578de21ddf4a97b41
Reggie Cole <reggie@robokind.com>
2019-01-21 15:15:29 -0600

    Got a base SCHEMA!!!! woohooo!!!!!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import JSONEditor from '@json-editor/json-editor';
-
+import schema from '../lib/courseSchema';
 JSONEditor.defaults.options.theme = 'bootstrap4';
 JSONEditor.defaults.options.iconlib = "materialicons";
 function initFirstTree() {
     let element = document.getElementById("tree");
-    let schema = {
-        type: "object",
-        properties: {
-            name: {
-                type: "string"
-            }
-        }
-    };
     let editor = new JSONEditor(element,{schema: schema});
     editor.setValue({name: "John Smith"});
 }
