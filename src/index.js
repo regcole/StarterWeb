3e632496449c678ddf8a168dcbf05385ef19e5e7
Reggie Cole <reggie@robokind.com>
2019-01-21 14:22:59 -0600

    Try using the json-editor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import JSONEditor from '@json-editor/json-editor';
+
+JSONEditor.defaults.options.theme = 'bootstrap4';
+JSONEditor.defaults.options.iconlib = "materialicons";
 function initFirstTree() {
-    let elm = document.getElementById("tree");
-    let schema = {"name":"string"};
+    let element = document.getElementById("tree");
+    let schema = {
+        type: "object",
+        properties: {
+            name: {
+                type: "string"
+            }
+        }
+    };
+    let editor = new JSONEditor(element,{schema: schema});
+    editor.setValue({name: "John Smith"});
 }
 
 window.onload = () => {
