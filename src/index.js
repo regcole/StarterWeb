f5fe9dd04f08b3292ffa552f2e06d2348f2b4538
Reggie Cole <reggie@robokind.com>
2019-01-22 10:17:13 -0600

    Remove default schema that had toolbox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import JSONEditor from '@json-editor/json-editor';
-import schema from '../lib/courseSchema';
+import schema from '../lib/base_course_schema';
 JSONEditor.defaults.options.theme = 'bootstrap4';
 // JSONEditor.defaults.options.iconlib = "materialicons";
 function initFirstTree() {
     let element = document.getElementById("tree");
     let editor = new JSONEditor(element,{schema: schema});
-    editor.setValue({name: "John Smith"});
+    editor.setValue({name: "Course 2"});
 }
 
 window.onload = () => {
